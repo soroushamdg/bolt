c263dcce6c540cf427b82111bf75f8b1c40f3741
soroushamdg <soroushmpour@gmail.com>
2021-08-22 23:59:09 +0430

    created activity object.

---
+++ include/Apps/BoltBoard/Activities/activity.h
@@ -0,0 +1,81 @@
+enum ActivityStatus
+{
+    create,
+    appear,
+    running,
+    pause,
+    resume,
+    destroy,
+
+}
+
+class Activity
+{
+private:
+    ActivityStatus current_status;
+
+public:
+    String id;
+    void changeStatusTo(ActivityStatus status)
+    {
+        current_status = status;
+        switch (status)
+        {
+        case create:
+            onCreate();
+            break;
+        case appear:
+            onAppear();
+            break;
+        case running:
+            break;
+        case pause:
+            onPause();
+            break;
+        case resume:
+            onResume();
+            break;
+        case destroy:
+            onDestroy();
+            break;
+        default:
+            break;
+        }
+    }
+
+    Activity()
+    {
+        changeStatusTo(ActivityStatus.create);
+    }
+    ~Activity()
+    {
+        changeStatusTo(ActivityStatus.destroy);
+    }
+
+    // Activity cycle functions
+    /*
+     Set defaults or load data from memory.
+    */
+    void onCreate() {}
+    /*
+     Set and make ready to display the activity.
+    */
+    void onAppear() {}
+    /*
+     Save latest status of activity and make ready to leave the screen.
+    */
+    void onPause() {}
+    /*
+     Reappear activity with latest status from memory.
+    */
+    void onResume() {}
+    /*
+     Save status to flash if needed and make ready to destroy from memory.
+    */
+    void onDestroy() {}
+
+    // Function Button functions
+    void onSinglePushFB(void) {}
+    void onHoldPushFB(void) {}
+    void onDoublePushFB(void) {}
+};
