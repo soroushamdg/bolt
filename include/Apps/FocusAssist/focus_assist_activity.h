40e287978bc1e68dbdc43e76ce922435f779d1d6
Soroush M Pour <soroushmpour@gmail.com>
2021-09-08 22:58:01 +0430

    added some more functions and stuff to focus assist activity.

--- include/Apps/FocusAssist/focus_assist_activity.h
+++ include/Apps/FocusAssist/focus_assist_activity.h
@@ -1,25 +1,58 @@
 #include <Apps/BoltBoard/Activities/activity.h>
 #include <Apps/FocusAssist/focus_assist_controller.h>
 
-class focusAssistActivity : public Activity
+class FocusAssistActivity : public Activity
 {
 private:
     FocusAssistController facontroller = FocusAssistController();
+    bool active = false;
 
 public:
-    focusAssistActivity()
+    FocusAssistActivity()
     {
-        Serial.println("Start creating...");
         id = "focus_assist_activity";
         setSpecialActivity();
+        active = false;
         changeStatusTo(ActivityStatus::created);
-        Serial.println("Finished creating...");
     }
 
-    void updateSettings() {}
+    void updateSettings()
+    {
+        // HERE LOAD SETTINGS AND FOCUS TIME FROM MEMORY - PREFRENCES
+    }
 
     void onCreate() override
     {
-        // HERE LOAD SETTINGS AND FOCUS TIME FROM MEMORY - PREFRENCES
+        updateSettings();
+        facontroller.setFocusTimeSecs(1 * 60); // 1 minute
+        facontroller.setFocusTimeSecs(1 * 60);
+    }
+
+    void startFocusAssist() {}
+    void pauseFocusAssist() {}
+    void stopFocusAssist() {}
+
+    void drawFocusBackground()
+    {
+        oledcontroller.fillBlueArea();
+        // draw and remove squares.
+    }
+
+    void drawFocusRemainTime()
+    {
+        uint16_t remainsecs = facontroller.focusTimeSec - facontroller.getTimerCount();
+        oledcontroller.printInCenter(String(String((int)remainsecs / 3600) + ((int)remainsecs / 60)),
+                                     2, 1, 0, -10); // hour : minute
+    }
+
+    void drawBreakRemainTime()
+    {
+        uint16_t remainsecs = facontroller.breakTimeSec - facontroller.getTimerCount();
+        oledcontroller.printInCenter(String(String((int)remainsecs / 3600) + ((int)remainsecs / 60)),
+                                     1, 1, 0, 10); // hour : minute
+    }
+
+    void updateScreen()
+    {
     }
 };
